6c7dcf5779fc55e5b6e223eebaa802911e0d7bd9
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-07-08 10:09:24 +0200

    Méthode save() Wallet

--- classes/Wallet.py
+++ classes/Wallet.py
@@ -1,11 +1,30 @@
 import uuid
+import json
+import os.path
 
 
 class Wallet:
 
-    unique_id = ""
-    balance = 0
-    history = []
+    def __init__(self):
+        self.unique_id = self.generate_unique_id()
+        self.balance = 0
+        self.history = []
 
     def generate_unique_id(self):
-        self.unique_id = uuid.uuid4()
+        return str(uuid.uuid4())
+
+    def add_balance(self, balance):
+        self.balance += balance
+
+    def sub_balance(self, balance):
+        self.balance -= balance
+
+    def send(self):
+        pass
+
+    def save(self):
+        content = json.dumps(self.__dict__)
+        path = os.path.join(os.getcwd(), "content\\wallets\\", self.unique_id)
+        with open("{}.json".format(path), "w+") as f:
+            f.write(content)
+            f.close()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from classes.Wallet import Wallet
 
 wallet = Wallet()
-wallet.generate_unique_id()
-print(wallet.unique_id)
+wallet.save()
