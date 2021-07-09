163ac916e86df55c898917b69d308388916b3e34
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-07-09 12:57:22 +0200

    Complétion main.py

--- main.py
+++ main.py
@@ -8,23 +8,41 @@ from classes import Chain, Wallet, Block
 
 # Simulation de transactions et comportements transverses
 def main():
+
+    print('\n*** Simulation de transactions '
+          'et comportements transverses ***\n')
+
     # Initialisation de la chaîne
+    print('\n* Initialisation de la chaîne *\n')
     chain = Chain()
+    print('--------------------------------')
+    print('Dernière transaction :', chain.last_transaction_number)
+    print('Liste des blocks :', " -> "
+          .join([block.hash for block in chain.blocks]))
+    print('--------------------------------')
+    input("\nAppuyer sur entrée pour continuer ...")
 
     # Initialisation de trois Wallets (méthode de génération d'un ID + save)
     path = os.path.join(os.getcwd(), "content\\wallets\\")
     if len(listdir(path)) < 3:
+        print('\n* Initialisation de trois Wallets *\n')
         wallet1 = Wallet()
         wallet2 = Wallet()
         wallet3 = Wallet()
+        utils.write_wallet_desc([wallet1, wallet2, wallet3])
+        input("\nAppuyer sur entrée pour continuer ...")
     # Ou récupération de trois wallets aléatoirement (méthode load)
     else:
+        print('\n* Récupération de trois wallets aléatoirement *\n')
         wallets_name = utils.get_wallets_name()
         wallet1 = Wallet(wallets_name.pop(randrange(len(wallets_name))))
         wallet2 = Wallet(wallets_name.pop(randrange(len(wallets_name))))
         wallet3 = Wallet(wallets_name.pop(randrange(len(wallets_name))))
+        utils.write_wallet_desc([wallet1, wallet2, wallet3])
+        input("\nAppuyer sur entrée pour continuer ...")
 
     # Initialisation de blocks
+    print('\n* Initialisation de blocks *\n')
     path = os.path.join(os.getcwd(), "content\\blocs\\")
     if len(listdir(path)) < 3:
         chain.add_block()
@@ -32,13 +50,19 @@ def main():
         chain.add_block()
 
     # Récupération du bloc principal
+    print('\n* Récupération du bloc principal *\n')
     block1 = chain.get_block("00")
+    utils.write_blocks_desc([block1])
+    input("\nAppuyer sur entrée pour continuer ...")
 
     # Récupération de deux autres blocs aléatoirement
     # (par la chaîne, et par le block directemment)
+    print('\n* Récupération de deux autres blocs aléatoirement *\n')
     blocks_hash = utils.get_blocks_hash()
     block2 = chain.get_block(blocks_hash.pop(randrange(len(blocks_hash))))
     block3 = Block(blocks_hash.pop(randrange(len(blocks_hash))))
+    utils.write_blocks_desc([block2, block3])
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 0. Récupération du dernier numéro de transaction
@@ -46,7 +70,9 @@ def main():
     print("\n0.------ "
           "Récupération du dernier numéro de transaction "
           "------\n")
-    print(chain.last_transaction_number)
+    print("N°", chain.last_transaction_number, "sur le block",
+          chain.find_transaction(chain.last_transaction_number).hash)
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 1. Transaction classique
@@ -56,22 +82,13 @@ def main():
           "------\n")
 
     transaction_amount = 10
-    print("Transaction de {} tokens".upper().format(transaction_amount))
-    print("Avant transaction :")
-    print("Émetteur ->", wallet1.unique_id, "->", wallet1.balance)
-    print("Récepteur ->", wallet2.unique_id, "->", wallet2.balance)
-
-    print(
-        "\nTransaction :",
-        chain.add_transaction(block2, wallet1, wallet2, transaction_amount),
-        "\n"
+    transaction = chain.add_transaction(
+        block2, wallet1, wallet2, transaction_amount
     )
-
-    print("Après transaction :")
-    print("Émetteur ->", wallet1.unique_id, "->", wallet1.balance,
-          "(-{})".format(transaction_amount))
-    print("Récepteur ->", wallet2.unique_id, "->", wallet2.balance,
-          "(+{})".format(transaction_amount))
+    utils.write_transaction_desc(
+        transaction, wallet1, wallet2, transaction_amount
+    )
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 2. Transaction trop importante pour un utilisateur (solde insuffisant)
@@ -79,7 +96,9 @@ def main():
     print("\n2. ------ "
           "Transaction n°2: solde insuffisant "
           "------\n")
+    print("Transaction de 500 tokens".upper())
     print(chain.add_transaction(block2, wallet1, wallet2, 500))
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 3. Surcharge d'un bloc pour tester la limite de place en octets
@@ -91,21 +110,27 @@ def main():
     result = None
     expected_response = "Transaction impossible: " \
                         "place non disponible sur le bloc choisi."
+    print("// Génération d'un grand nombre de transaction sur le block {} "
+          "\\\\\n".format(block1.hash))
     while result != expected_response:
         result = chain.add_transaction(
             block1, wallet1, wallet2, 0)
     print(expected_response)
     print("Poids du block: {} octets".format(block1.get_weight()))
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 4. Transactions avec des wallets invalides
     #
     print("\n4. ------ "
-          "Transaction n°4: wallet inexistant "
+          "Transaction n°4: wallets inexistants "
           "------\n")
-    print(chain.add_transaction(block2, Wallet("Test"), wallet2, 500))
-    print(chain.add_transaction(block2, wallet1, Wallet("Test"), 500))
-    print(chain.add_transaction(block2, Wallet("Test"), Wallet("Test"), 500))
+    random_wallet = Wallet("Test")
+    utils.write_wallet_desc([random_wallet])
+    print(chain.add_transaction(block2, random_wallet, wallet2, 500))
+    print(chain.add_transaction(block2, wallet1, random_wallet, 500))
+    print(chain.add_transaction(block2, random_wallet, random_wallet, 500))
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 5. Deuxième transaction classique avec autre block et wallet
@@ -113,23 +138,13 @@ def main():
     print("\n5. "
           "------ Transaction n°5: "
           "------\n")
-
-    print("Transaction de {} tokens".upper().format(transaction_amount))
-    print("Avant transaction :")
-    print("Émetteur ->", wallet3.unique_id, "->", wallet3.balance)
-    print("Récepteur ->", wallet2.unique_id, "->", wallet2.balance)
-
-    print(
-        "\nTransaction :",
-        chain.add_transaction(block3, wallet3, wallet2, transaction_amount),
-        "\n"
+    transaction = chain.add_transaction(
+        block3, wallet3, wallet2, transaction_amount
     )
-
-    print("Après transaction :")
-    print("Émetteur ->", wallet3.unique_id, "->", wallet3.balance,
-          "(-{})".format(transaction_amount))
-    print("Récepteur ->", wallet2.unique_id, "->", wallet2.balance,
-          "(+{})".format(transaction_amount))
+    utils.write_transaction_desc(
+        transaction, wallet1, wallet2, transaction_amount
+    )
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 6. Reconstruction de la chaine lors de l'initialisation d'un objet Chain
@@ -140,6 +155,7 @@ def main():
     del chain
     chain = Chain()
     print(" -> ".join([block.hash for block in chain.blocks]))
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 7. Récupération du dernier numéro de transaction
@@ -148,7 +164,9 @@ def main():
     print("\n7. ------ "
           "Récupération du dernier numéro de transaction "
           "------\n")
-    print(chain.last_transaction_number)
+    print("N°", chain.last_transaction_number, "sur le block",
+          chain.find_transaction(chain.last_transaction_number).hash)
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 8. Récupération d'un block par le numéro de transaction
@@ -159,6 +177,7 @@ def main():
     print(chain.find_transaction(chain.last_transaction_number))
     print(chain.find_transaction(-1))
     print(chain.find_transaction(chain.last_transaction_number + 1))
+    input("\nAppuyer sur entrée pour continuer ...")
 
     #
     # 9. Récupération d'une transaction par le numéro depuis le block
@@ -169,6 +188,7 @@ def main():
     print(block3.get_transaction(chain.last_transaction_number))
     print(block3.get_transaction(-1))
     print(block3.get_transaction(chain.last_transaction_number + 1))
+    input("\nAppuyer sur entrée pour continuer ...")
 
 
 if __name__ == '__main__':
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import os.path
 from os import listdir
 
+
 # FONCTIONS REUTILISEES PLUSIEURS FOIS DANS LE CODE
 
 
@@ -22,3 +23,46 @@ def get_blocks_hash():
         if not os.path.isdir(path + "\\" + file) and file != "00.json":
             files.append(os.path.splitext(file)[0])
     return files
+
+
+# Fonction d'affichage du détail d'un ou plusieurs wallets
+def write_wallet_desc(wallets):
+    for wallet in wallets:
+        print('--------------------------------')
+        print("ID -", wallet.unique_id)
+        print("Solde -", wallet.balance, "tokens")
+        print("Nombre de transactions -", len(wallet.history))
+        print('--------------------------------')
+
+
+# Fonction d'affichage du détail d'un ou plusieurs blocks
+def write_blocks_desc(blocks):
+    for block in blocks:
+        print('--------------------------------')
+        print('Hash -', block.hash)
+        print('Base du hash -', block.base_hash)
+        print('Cohérence Hash / Base du hash -', block.check_hash())
+        print('Parent -', block.parent_hash)
+        print("Nombre de transactions -", len(block.transactions))
+        print('--------------------------------')
+
+
+# Fonction d'affichage du détail d'une transaction, avec émetteur et récepteur
+# ainsi que le montant
+def write_transaction_desc(transaction, transmitter, receiver, amount):
+    print("Transaction de {} tokens\n".upper().format(amount))
+    print('--------------------------------')
+    print("Avant transaction :")
+    print("Émetteur ->", transmitter.unique_id, "->", transmitter.balance)
+    print("Récepteur ->", receiver.unique_id, "->", receiver.balance)
+    print('--------------------------------')
+
+    print("\nTransaction :", transaction, "\n")
+
+    print('--------------------------------')
+    print("Après transaction :")
+    print("Émetteur ->", transmitter.unique_id, "->", transmitter.balance,
+          "(-{})".format(amount))
+    print("Récepteur ->", receiver.unique_id, "->", receiver.balance,
+          "(+{})".format(amount))
+    print('--------------------------------')
